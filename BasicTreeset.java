50aa8f17a484dd76fe17e62d0d9bb9e90249d079
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-09-20 22:24:32 +0530

    Add files via upload

---
+++ BasicTreeset.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hashing_apna_college;
+import java.util.*;
+public class BasicTreeset {
+    public static void main(String[]args)
+    {
+        TreeSet<Integer> ts=new TreeSet<>();
+        ts.add(2);
+        ts.add(1);
+        ts.add(3);
+        ts.add(5);
+        ts.add(4);
+        System.out.println(ts);
+    }
+}
